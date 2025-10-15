61a59fdaa1401cbb82b94bca47c358c4b1e7d28b
385858 <a385858@uach.mx>
2025-10-15 07:42:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("Mi aplicacion para calcular el area figuras geométricas")
+st.title("Mi aplicacion para calcular el area y perímetro de figuras geométricas")
 import math
 # Widget para ingresar el radio
 st.title("Cálculo de área de un círculo")
@@ -11,6 +11,5 @@ area = math.pi * radio**2
 # Mostrar resultado
 st.write(f"El area del circulo con radio {radio} es: {area: .2f}")
 
-# Widget para ingresar base y altura
-base= st.slider("Selecciona la base", 0.0, 10.0, 5.0)
-altura= st.slider("Selecciona la altura", 0.0, 10.0, 5.0)
+# 1. Selección de la figura
+figura = st.selectbox("Selecciona una figura geométrica:", ("Círculo", "Triángulo", "Rectángulo", "Cuadrado"))
